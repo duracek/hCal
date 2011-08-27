05568e64381b2d45956390ffee75d50e237c2267
unknown <Administrator@.slemish.local>
2011-08-27 12:01:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HCal::Application.config.secret_token = '13ec01c6fa5cc45d17f44692b0b3fb19dab6bb7b6a19e424819cca68ad0006eeea6b630e551c0a6d22e07221b79018be19c284d7eecd9692f1453ce111b1931c'
